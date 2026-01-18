6437aa52892596335a3415a0b65a67127ffcbad8
SayedMohammedYahiyaKhadri <sayedyahiya18@gmail.com>
2026-01-18 20:43:08 +0530

    Update celsius_fahrenheit.py

--- celsius_fahrenheit.py
+++ celsius_fahrenheit.py
@@ -1,6 +1,9 @@
-def celsius_to_fahrenheit(c):
-    return (c * 9/5) + 32
-
-if __name__ == "__main__":
-    c = 25
-    print("Fahrenheit:", celsius_to_fahrenheit(c))
+import sys
+if len(sys.argv) > 1:
+    celsius = float(sys.argv[1]) 
+    print("User provided temperature:") 
+else:
+    celsius = 25  
+    print("No input given - using default temperature:")
+fahrenheit = (celsius * 9/5) + 32
+print("Script Name:", script_name) print("Celsius:", celsius) print("Fahrenheit:", fahrenheit)
