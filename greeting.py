9c8509e4e079a1c5a54c2038ff4dc1b46257067d
SayedMohammedYahiyaKhadri <sayedyahiya18@gmail.com>
2026-01-18 20:08:35 +0530

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,2 +1,10 @@
-name=input("Enter your name :")
-print(f"Hello, {name}! Welcome!")
+import sys
+
+if len(sys.argv) == 2:
+    celsius = float(sys.argv[1])
+else:
+    celsius = 25
+
+fahrenheit = (celsius * 9/5) + 32
+print("Celsius:", celsius)
+print("Fahrenheit:", fahrenheit)
